3735e518fa2b187679a75344fbf7dd374e1c0d79
Gustavo <sgkenzo212007@gmail.com>
2025-07-31 16:38:05 -0300

    Implementar função para atualizar a lista de amigos

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-let nomes = [];
+let amigos = [];
 function adicionarAmigo() {
     if (document.querySelector('input').value.trim() == '') {
-        alert('Por favor, insira um nome.');
+        alert('Por favor, insira um nome válido.');
         limparTela();
     } else {
         let amigo = document.querySelector('input').value;
-        nomes.push(amigo);
+        amigos.push(amigo);
+        atualizarListaAmigos();
         limparTela();
     }
 }
 function limparTela() {
     document.querySelector('input').value = '';
+}
+
+function atualizarListaAmigos() {
+    let listaAmigosHTML = document.querySelector('#listaAmigos');
+    listaAmigosHTML.innerHTML = "";
+
+    for (let amigo of amigos) {
+        let novoAmigo = document.createElement('li');
+        novoAmigo.textContent = amigo;
+        listaAmigosHTML.appendChild(novoAmigo);
+    }
 }
