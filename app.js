320a7faeea7f692ce62c3398d797b25587f493f1
Gustavo <sgkenzo212007@gmail.com>
2025-07-31 17:53:34 -0300

    Implementar função para sortear os amigos.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function adicionarAmigo() {
     } else {
         let amigo = document.querySelector('input').value;
         amigos.push(amigo);
-        atualizarListaAmigos();
+        atualizarListaAmigos(amigos);
         limparTela();
     }
 }
@@ -14,13 +14,31 @@ function limparTela() {
     document.querySelector('input').value = '';
 }
 
-function atualizarListaAmigos() {
+function atualizarListaAmigos(amigo) {
     let listaAmigosHTML = document.querySelector('#listaAmigos');
-    listaAmigosHTML.innerHTML = "";
+    listaAmigosHTML.innerHTML = '';
 
-    for (let amigo of amigos) {
+    if (Array.isArray(amigo)) {
+        for (let i = 0; i < amigos.length; i++) {
+            let novoAmigo = document.createElement('li');
+            novoAmigo.textContent = amigos[i];
+            listaAmigosHTML.appendChild(novoAmigo);
+        }
+    }
+    else {
         let novoAmigo = document.createElement('li');
-        novoAmigo.textContent = amigo;
+        novoAmigo.textContent = `O amigo secreto sorteado é: ${amigo}`;
         listaAmigosHTML.appendChild(novoAmigo);
+    }
+
+}
+
+function sortearAmigo() {
+    if (amigos.length == 0) {
+        console.log('Está vazio');
+    } else {
+        let index = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[index];
+        atualizarListaAmigos(amigoSorteado);
     }
 }
